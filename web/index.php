472b8540b3cc532cbb1b5a0b3d162e34744b5464
JorgeArgu1999 <jorge.arguello1999@gmail.com>
2022-01-06 23:43:07 -0500

    Se comenzo a trabajar en la pagina web, se creara una pagina para todo lo que no tenga que ver con la aplicación web y una pagina especial para la pagina web

--- web/index.php
+++ web/index.php
@@ -3,12 +3,26 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Manejador de Productos</title>
+	<link rel="stylesheet" href="estilos.css">
 </head>
 <body>
+	<div class="barra_menu">
+		<h1>Manegador de Productos</h1>
+		<ul>
+			<li><a href="#web">Aplicación Web</a></li>
+			<li><a href="#aplicacion">Aplicación CLI</a></li>
+			<li><a href="#gui">Aplicación GUI</a></li>
+			<li><a href="#documentacion">Documentación</a></li>
+			<li><a href="#desarrollo">Desarrollo</a></li>
+		</ul>
+	</div>
 
-	<h1>Manegador de Productos</h1>
+	<div class="footer">
+		<h1>footer</h1>
+	</div>
+	
 	<p>Este es un manejador de productos (prototipo) para negocios pequeños</p>
-	<?php include "database.php"; ?>
+	<?php require"database.php"; ?>
 
 </body>
 </html>
