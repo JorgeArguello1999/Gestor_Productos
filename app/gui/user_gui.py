74a9a9e06e00cffa0b1f5ff767c511652fdb573f
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2022-06-27 19:50:32 -0500

    fix: Activamos el boton de recargar en la GUI refactor: Arreglamos el cargador de la tabla

--- app/gui/user_gui.py
+++ app/gui/user_gui.py
@@ -1,5 +1,6 @@
 import Conectors.db_connect as db_connect
 import sys
+import time
 
 # Modulos de Qt5
 from PyQt5 import uic
@@ -26,7 +27,12 @@ class interface(QMainWindow):
         self.boton_registro.clicked.connect(self.login)
 
     def cargador_tabla_componentes(self):
-        productos= self.productos.listar()
+        """
+        Por cada llamado a la funcion la variable productos almacena nuevos datos
+        si la llamamos como propiedad de clase, esta no almacena los nuevos datos.
+        """
+        productos= db_connect.productos()
+        productos= productos.listar()
         fila=0
         self.tabla_productos.setRowCount(len(productos))
         for individual in productos:
@@ -41,7 +47,6 @@ class interface(QMainWindow):
         self.seleccionar_productos.clicked.connect(self.seleccionar)
         # Cargamos boton de cargar
         self.boton_productos.clicked.connect(self.cargador_tabla_componentes)
-        self.limpieza()
 
     def eleccion_producto(self):
         try:
@@ -100,33 +105,37 @@ class interface(QMainWindow):
             # Inicamos el componente
             self.cargador_tabla_componentes()
             self.menu_opciones()
+            # Llamamos al come procesador
         else:
             validacion= "[No Autorizado]" 
         self.comprobador.setText(validacion)
 
     # Boton de seleccionar
     def seleccionar(self):
-        global salida
-        salida= self.productos.listar()
-        for i in salida:
-            if i[0]==codigoSeleccion:
-                codigo=str(i[0])
-                global nombreSeleccion
-                nombreSeleccion=str(i[1])
-                # Mostramos en la opcion de Editar
-                self.id_editar.setText(codigo)
-                self.nombre_editar.setText(nombreSeleccion)
-                # Mostramos en la opcion de Eliminar
-                self.id_eliminar.setText(codigo)
-                self.nombre_eliminar.setText(nombreSeleccion)
-                return nombreSeleccion
+        try: 
+            global salida
+            salida= self.productos.listar()
+            for i in salida:
+                if i[0]==codigoSeleccion:
+                    codigo=str(i[0])
+                    global nombreSeleccion
+                    nombreSeleccion=str(i[1])
+                    # Mostramos en la opcion de Editar
+                    self.id_editar.setText(codigo)
+                    self.nombre_editar.setText(nombreSeleccion)
+                    # Mostramos en la opcion de Eliminar
+                    self.id_eliminar.setText(codigo)
+                    self.nombre_eliminar.setText(nombreSeleccion)
+                    return nombreSeleccion
+        except:
+            self.mensaje_productos.setText("[ID no seleccionado]")
 
     def insertar(self):
         # Obtenemos la entrada del usuario
-        nombre= str(self.nombre_insertar.text())
-        cantidad= int(self.cantidad_insertar.text())
-        precio= float(self.precio_insertar.text())
         try:
+            nombre= str(self.nombre_insertar.text())
+            cantidad= int(self.cantidad_insertar.text())
+            precio= float(self.precio_insertar.text())
             # Evitamos que ingrese algo con el mismo nombre o mismo id
             self.productos.insertar(nombre, cantidad, precio)
             self.cargador_tabla_componentes()
@@ -135,24 +144,23 @@ class interface(QMainWindow):
             self.mensaje_insertar.setText("[ Error 104 ]")
 
     def editar(self):
-        codigo= self.id_editar.text()
-        nombre= str(self.nombreEntrada_editar.text())
-        cantidad= int(self.cantidad_editar.text())
-        precio= float(self.precio_editar.text())
         try:
-           self.productos.editar(codigo, nombre, cantidad, precio)
-           self.cargador_tabla_componentes()
-           self.mensaje_editar.setText("[Realizado con Exito]")
+            codigo= self.id_editar.text()
+            nombre= str(self.nombreEntrada_editar.text())
+            cantidad= int(self.cantidad_editar.text())
+            precio= float(self.precio_editar.text())
+            self.productos.editar(codigo, nombre, cantidad, precio)
+            self.mensaje_editar.setText("[Realizado con Exito]")
+            self.cargador_tabla_componentes()
         except:
             self.mensaje_editar.setText("[Error 104]")
 
     def eliminar(self):
-        codigo= self.id_editar.text()
-        nombre= self.nombreEntrada_editar.text()
         try:
+            codigo= self.id_editar.text()
             self.productos.eliminar(codigo)
-            self.cargador_tabla_componentes()
             self.mensaje_eliminar.setText("[Borrado con Exito]")
+            self.cargador_tabla_componentes()
         except:
             self.mensaje_eliminar.setText("[Error ID no seleccionado]")
 
